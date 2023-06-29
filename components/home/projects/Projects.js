3fbd98d9f2a4e37f461384e19363f9c6d647fa24
ahmed iblao <ahmediblao53@gmail.com>
2023-06-29 19:23:40 +0300

    Merge remote changes

--- components/home/projects/Projects.js
+++ components/home/projects/Projects.js
@@ -17,79 +17,79 @@ export const Projects = () => {
 };
 
 const projects = [
-  {
-    title: "Paint.app",
-    imgSrc: "project-imgs/example-project.jpg",
-    code: "https://www.github.com",
-    projectLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    tech: ["Flutter", "MUI", "Python", "FastAPI"],
-    description:
-      "A real-time coaching app for students learning to paint. This app is my baby, designed and built on my own.",
-    modalContent: (
-      <>
-        <p>
-          Pain.app is a real-time coaching app for students learning to paint.
-          This app is my baby, designed and built on my own.
-        </p>
-        <p>
-          The tech stack is based on top of Flutter for the mobile app,
-          connected to a Python & FastAPI backend, with data stored in Postgres,
-          deployed on Heroku.
-        </p>
-        <p>
-          Because this isn&apos;t real, here&apos;s some gibberish to fill space{" "}
-          {":)"}
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
-          quia officia odio nulla consectetur aperiam ad tempora magni magnam
-          nesciunt.
-        </p>
-        <p>
-          Fuga id sapiente facere ipsa eius exercitationem officiis deleniti,
-          rerum dolorum. Deserunt soluta modi culpa animi.
-        </p>
-      </>
-    ),
-  },
-  {
-    title: "The Canvas Club",
-    imgSrc: "project-imgs/example-project.jpg",
-    code: "https://www.github.com",
-    projectLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    tech: ["Node", "Express", "Postgres", "Kafka", "Redis"],
-    description:
-      "A social community for painters to connect with others in their community. I handle everything backend (50K monthly active users).",
-    modalContent: (
-      <>
-        <p>
-          The Canvas Club is a social community for painters to connect with
-          others in their community.
-        </p>
-        <p>
-          I work primarily on the backend, a collection of Node & Express
-          microservices. Data is stored primarily in Postgres & cached in Redis.
-        </p>
-        <p>
-          The team in total consists of 5 developers. This is a passion project
-          for all of us.
-        </p>
-        <p>
-          Because this isn&apos;t real, here&apos;s some gibberish to fill space{" "}
-          {":)"}
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
-          quia officia odio nulla consectetur aperiam ad tempora magni magnam
-          nesciunt.
-        </p>
-        <p>
-          Fuga id sapiente facere ipsa eius exercitationem officiis deleniti,
-          rerum dolorum. Deserunt soluta modi culpa animi.
-        </p>
-      </>
-    ),
-  },
+  // {
+  //   title: "Paint.app",
+  //   imgSrc: ".jpg",
+  //   code: "https://www.github.com",
+  //   projectLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  //   tech: ["Flutter", "MUI", "Python", "FastAPI"],
+  //   description:
+  //     "A real-time coaching app for students learning to paint. This app is my baby, designed and built on my own.",
+  //   modalContent: (
+  //     <>
+  //       <p>
+  //         Pain.app is a real-time coaching app for students learning to paint.
+  //         This app is my baby, designed and built on my own.
+  //       </p>
+  //       <p>
+  //         The tech stack is based on top of Flutter for the mobile app,
+  //         connected to a Python & FastAPI backend, with data stored in Postgres,
+  //         deployed on Heroku.
+  //       </p>
+  //       <p>
+  //         Because this isn&apos;t real, here&apos;s some gibberish to fill space{" "}
+  //         {":)"}
+  //       </p>
+  //       <p>
+  //         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
+  //         quia officia odio nulla consectetur aperiam ad tempora magni magnam
+  //         nesciunt.
+  //       </p>
+  //       <p>
+  //         Fuga id sapiente facere ipsa eius exercitationem officiis deleniti,
+  //         rerum dolorum. Deserunt soluta modi culpa animi.
+  //       </p>
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: "The Canvas Club",
+  //   imgSrc: "project-imgs/example-project.jpg",
+  //   code: "https://www.github.com",
+  //   projectLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  //   tech: ["Node", "Express", "Postgres", "Kafka", "Redis"],
+  //   description:
+  //     "A social community for painters to connect with others in their community. I handle everything backend (50K monthly active users).",
+  //   modalContent: (
+  //     <>
+  //       <p>
+  //         The Canvas Club is a social community for painters to connect with
+  //         others in their community.
+  //       </p>
+  //       <p>
+  //         I work primarily on the backend, a collection of Node & Express
+  //         microservices. Data is stored primarily in Postgres & cached in Redis.
+  //       </p>
+  //       <p>
+  //         The team in total consists of 5 developers. This is a passion project
+  //         for all of us.
+  //       </p>
+  //       <p>
+  //         Because this isn&apos;t real, here&apos;s some gibberish to fill space{" "}
+  //         {":)"}
+  //       </p>
+  //       <p>
+  //         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
+  //         quia officia odio nulla consectetur aperiam ad tempora magni magnam
+  //         nesciunt.
+  //       </p>
+  //       <p>
+  //         Fuga id sapiente facere ipsa eius exercitationem officiis deleniti,
+  //         rerum dolorum. Deserunt soluta modi culpa animi.
+  //       </p>
+  //     </>
+  //   ),
+  // },
   {
     title: "Ridezy",
     imgSrc: "project-imgs/web1.jpg",
