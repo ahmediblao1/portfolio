25b7c5dc851635cfd1ca2e0c2e5800d19a53d9d4
ahmed iblao <98063069+ahmediblao1@users.noreply.github.com>
2024-02-09 20:58:21 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ weight: ["100","100", "200", "400", "700", "900"] });
+const poppins = Poppins({ weight: ["100","200", "400", "700", "900"] });
 
 export default function App({ Component, pageProps }) {
   return (
